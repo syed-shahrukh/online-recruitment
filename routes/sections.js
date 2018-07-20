ca01b843a17bcac31bd216c48384cc80fb159413
Majid Zulfiqar <mzulfiqar@xavor.com>
2018-07-20 17:51:16 +0500

    just for checking

--- routes/sections.js
+++ routes/sections.js
@@ -10,7 +10,7 @@ router.get('/', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-  
+  console.log(req.body);
   const { error } = validate(req.body); 
   
   if (error) return res.status(400).send(error.details[0].message);
